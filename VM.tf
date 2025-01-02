beabaa0a8757d8bd565f7d35a21e418a62f505d4
Al Wilkinson <alastair.wilkinson@wkconsulting.com.au>
2025-01-02 12:14:00 +1000

    Add key pair

--- VM.tf
+++ VM.tf
@@ -13,9 +13,15 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name = "deployer-key"
+  public_key = file("~/.ssh/flask-demo-vm.pub")
+}
+
 resource "aws_instance" "demo-vm" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "FlaskDemo"
